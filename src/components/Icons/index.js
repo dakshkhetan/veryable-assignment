c4fd2a97131ad9643f3e4e6e89a78bb6c8fc1d71
Daksh Khetan <2012daksh@gmail.com>
2023-05-30 13:42:51 -0500

    add icon components

---
+++ src/components/Icons/index.js
@@ -0,0 +1,6 @@
+import ExpandMoreIcon from "./ExpandMoreIcon";
+import ExpandLessIcon from "./ExpandLessIcon";
+import GroupIcon from "./GroupIcon";
+import UserIcon from "./UserIcon";
+
+export { ExpandMoreIcon, ExpandLessIcon, GroupIcon, UserIcon };
